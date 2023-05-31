4dc0763a5603f7672653908a706bd6b7dbfd0f67
hyunseok.kim <dragmove@naver.com>
2023-06-01 01:12:25 +0900

    :white_check_mark: add test cases related to utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -5,3 +5,8 @@ export const isServer: boolean = typeof window === "undefined";
 export const range = (start: number, end: number): Array<number> => {
   return Array.from(Array(Math.abs(end - start) + 1), (_, i) => start + i);
 };
+
+export function isDefined<T>(val: T): val is NonNullable<T> {
+  if (val == null || typeof val === "undefined") return false;
+  return true;
+}
